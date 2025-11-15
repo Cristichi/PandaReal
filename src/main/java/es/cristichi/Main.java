da1fe733ff2881a293eab0fbf3e07865424e4cd9
Cristichi <cristichi@hotmail.es>
2025-11-15 11:10:04 +0100

    ¡Añadida configuración de jugadores y rondas!

--- src/main/java/es/cristichi/Main.java
+++ src/main/java/es/cristichi/Main.java
@@ -3,12 +3,37 @@ package es.cristichi;
 import es.cristichi.juego.Jugador;
 import es.cristichi.juego.Partida;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        Partida partida = new Partida(10,
-                new Jugador("Cristichi"),
-                new Jugador("Jugador 2", false),
-                new Jugador("Jugador 3", false));
+        // Vamos a preguntar al usuario el número de jugadores y sus nombres, y cuáles son humanos/CPU
+        ArrayList<Jugador> jugadores = new ArrayList<>();
+        String input = "";
+        while (!input.equals("jugar")) {
+            System.out.print("Introduce el nombre del siguiente jugador (o 'jugar' para empezar la partida): ");
+            input = System.console().readLine();
+            if (!input.equals("jugar")) {
+                System.out.print("¿Es un jugador humano? (S/n): ");
+                String esHumano = System.console().readLine();
+                boolean humano = !esHumano.equalsIgnoreCase("n");
+                Jugador nuevo = new Jugador(input, humano);
+                jugadores.add(nuevo);
+                System.out.printf("Añadido %s%n", nuevo);
+            }
+        }
+        // Preguntar número de rondas, 10 por defecto
+        int numRondas = 10;
+        System.out.print("¿Cuántas rondas quieres jugar? (Pulsa \"INTRO\" para 10 rondas): ");
+        String rondasInput = System.console().readLine();
+        if (!rondasInput.isEmpty()) {
+            try {
+                numRondas = Integer.parseInt(rondasInput);
+            } catch (NumberFormatException ignored) {
+            }
+        }
+
+        Partida partida = new Partida(numRondas, jugadores.toArray(new Jugador[0]));
         partida.start();
     }
 }
